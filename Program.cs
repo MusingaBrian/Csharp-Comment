35f4491004b944cfc773e31c711c8416ad7d9cba
MusingaBrian <musingabrians@gmail.com>
2024-06-26 13:48:02 +0300

    Add More sudents and update the if statements

--- Program.cs
+++ Program.cs
@@ -5,9 +5,13 @@ int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
 int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
 int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };
 
 // Student names
-string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
 
 int[] studentScores = new int[10];
 
@@ -35,6 +39,25 @@ foreach (string name in studentNames)
     else if (currentStudent == "Logan")
         // assign Logan's scores to the studentScores array 
         studentScores = loganScores;
+
+    else if (currentStudent == "Becky")
+        // assign Becky's scores to the studentScores array 
+        studentScores = loganScores;
+
+    else if (currentStudent == "Chris")
+        // assign Chris's scores to the studentScores array 
+        studentScores = loganScores;
+
+    else if (currentStudent == "Eric")
+        // assign Eric's scores to the studentScores array 
+        studentScores = loganScores;
+
+    else if (currentStudent == "Gregor")
+        // assign Gregor's scores to the studentScores array 
+        studentScores = loganScores;
+
+    else
+        continue;
 
     // initialize/reset the sum of scored assignments
     int sumAssignmentScores = 0;
